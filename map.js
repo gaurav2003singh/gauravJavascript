15603269b3314f4952e5586db7dbe3703ad93a49
Gaurav Singh <www.gschauhan2003@gmail.com>
2025-05-08 12:39:40 +0530

    gtu

--- map.js
+++ map.js
@@ -1,3 +1,14 @@
 const arr = [2,3,4,5,6]
-const output = arr.map((n)=>n*2)
-console.log(output)
+const output2 = arr.map((n)=>n*2)
+console.log(output2)
+
+const users = [
+  { firstName: "gaurav", age: 26 },
+  { firstName: "arav", age: 29 },
+  { firstName: "saurav", age: 31 },
+  { firstName: "raju", age: 26 },
+];
+
+const output = users.filter((x) => x.age < 30);
+
+console.log(output);
